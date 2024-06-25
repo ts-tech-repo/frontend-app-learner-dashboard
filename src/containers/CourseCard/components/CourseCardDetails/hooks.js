47e6855932c48f231615c4fd3391708cd251d3d7
ts-tech-repo <152260343+ts-tech-repo@users.noreply.github.com>
2024-06-25 12:53:38 +0530

    changed to courseData from courseId

--- src/containers/CourseCard/components/CourseCardDetails/hooks.js
+++ src/containers/CourseCard/components/CourseCardDetails/hooks.js
@@ -58,7 +58,7 @@ export const useCardDetailsData = ({ cardId }) => {
     openSessionModal,
     courseNumber,
     changeOrLeaveSessionMessage: formatMessage(messages.changeOrLeaveSessionButton),
-    courseId : courseData.courseId
+    courseId : courseData
   };
 };
 
