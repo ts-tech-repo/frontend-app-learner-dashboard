ccddce3cdb0f7a8cf940988061357cd2e939ca9c
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-09-02 14:28:28 +0530

    removed initial title as iitk to implement for cmu and split condition for title

--- src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
@@ -23,16 +23,15 @@ export const CollapseMenuBody = ({ isOpen }) => {
 
   const exploreCoursesClick = findCoursesNavDropdownClicked(urls.baseAppUrl(courseSearchUrl));
 
-  const [quickLinkText, setQuickLinkText] = React.useState("IIT Kanpur eMasters Degree");
+  const [quickLinkText, setQuickLinkText] = React.useState("");
 
   React.useEffect(() => {
     const interval = setInterval(() => {
         const quickLinkElement = document.querySelector('.quick-link-tag');
         if (quickLinkElement) {
             const fullText = quickLinkElement.textContent.trim();
-            const textBeforeHyphen = fullText.includes('-') ? fullText.split('-')[0] : fullText;
             if (!fullText.startsWith('QUINCE') && !fullText.startsWith('EMIITK')) {
-                setQuickLinkText(`eMasters in ${textBeforeHyphen} `);
+                setQuickLinkText(`eMasters in ${fullText} `);
             }
             clearInterval(interval);
         }
