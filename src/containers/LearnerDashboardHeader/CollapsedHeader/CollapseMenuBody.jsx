6fde266605269e5da1b61ee59e471b50ccf344f5
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-09-02 16:14:10 +0530

    removed emaster in text

--- src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
@@ -30,9 +30,9 @@ export const CollapseMenuBody = ({ isOpen }) => {
         const quickLinkElement = document.querySelector('.quick-link-tag');
         if (quickLinkElement) {
             const fullText = quickLinkElement.textContent.trim();
-            if (!fullText.startsWith('QUINCE') && !fullText.startsWith('EMIITK')) {
-                setQuickLinkText(`eMasters in ${fullText} `);
-            }
+            // if (!fullText.startsWith('QUINCE') && !fullText.startsWith('EMIITK')) {
+                setQuickLinkText(` ${fullText} `);
+            // }
             clearInterval(interval);
         }
     }, 100);
