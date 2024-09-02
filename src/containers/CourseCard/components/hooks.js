4c6aedc8dd5e0fd5bedca0cbdc9d4283bb314c78
manoj.p3 <manoj.p@talentsprint.com>
2024-09-02 14:30:43 +0530

    Handled disabled resume button when Masquerading

--- src/containers/CourseCard/components/hooks.js
+++ src/containers/CourseCard/components/hooks.js
@@ -15,7 +15,7 @@ export const useActionDisabledState = (cardId) => {
   const disableBeginCourse = isMasquerading
   ? startDate > new Date()
   : !homeUrl || (!hasAccess || (isAudit && isAuditAccessExpired));
-  const disableResumeCourse = !resumeUrl || (isMasquerading || !hasAccess || (isAudit && isAuditAccessExpired));
+  const disableResumeCourse = isMasquerading ? !resumeUrl : !resumeUrl || (!hasAccess || (isAudit && isAuditAccessExpired));
   const disableViewCourse = !hasAccess || (isAudit && isAuditAccessExpired);
   const disableSelectSession = !isEntitlement || isMasquerading || !hasAccess || (!canChange || !hasSessions);
   const disableUpgradeCourse = !upgradeUrl || (isMasquerading && !canUpgrade);
