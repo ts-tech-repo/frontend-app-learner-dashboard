bccebd2e1055abc71fe1a619982486058846c59a
sohail.m <sohail.m@talentsprint.com>
2024-09-12 19:09:00 +0530

    accepts hypen for mobile view

--- src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
@@ -31,9 +31,8 @@ export const ExpandedHeader = () => {
         const quickLinkElement = document.querySelector('.quick-link-tag');
         if (quickLinkElement) {
             const fullText = quickLinkElement.textContent.trim();
-            const textBeforeHyphen = fullText.includes('-') ? fullText.split('-')[0] : fullText;
-            setEmasterTitle(`eMasters in ${textBeforeHyphen}`);
-            setCertificateTitle(textBeforeHyphen);
+            setEmasterTitle(`eMasters in ${fullText}`);
+            setCertificateTitle(fullText);
             clearInterval(interval);
         }
     }, 100);
