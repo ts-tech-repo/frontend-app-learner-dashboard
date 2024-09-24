e5e18336c2e3ea8787b5a00faf078b6aa46cb0b8
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-09-24 17:40:49 +0530

    dont show hypen only for emaster

--- src/containers/CourseCard/components/CourseCardTitle.jsx
+++ src/containers/CourseCard/components/CourseCardTitle.jsx
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 import track from 'tracking';
 import { reduxHooks } from 'hooks';
 import useActionDisabledState from './hooks';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import messages from '../../LearnerDashboardHeader/messages';
 
 const { courseTitleClicked } = track.course;
 
 export const CourseCardTitle = ({ cardId }) => {
+  const { formatMessage } = useIntl();
+  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
   const { courseName } = reduxHooks.useCardCourseData(cardId);
   const { homeUrl } = reduxHooks.useCardCourseRunData(cardId);
+  const extractedCourse = courseName.includes("-") ? courseName.split(/-(.+)/)[1].trim() : courseName;
 
   const handleTitleClicked = reduxHooks.useTrackCourseEvent(
     courseTitleClicked,
@@ -20,7 +25,9 @@ export const CourseCardTitle = ({ cardId }) => {
   return (
     <h3>
       {disableCourseTitle ? (
-        <span className="course-card-title" data-testid="CourseCardTitle">{courseName}</span>
+        <span className="course-card-title" data-testid="CourseCardTitle">
+          {siteNameMessage === "IIT Kanpur eMasters Degree" ? courseName : extractedCourse}
+        </span>
       ) : (
         <a
           href={homeUrl}
@@ -28,7 +35,7 @@ export const CourseCardTitle = ({ cardId }) => {
           data-testid="CourseCardTitle"
           onClick={handleTitleClicked}
         >
-          {courseName}
+          {extractedCourse}
         </a>
       )}
     </h3>
