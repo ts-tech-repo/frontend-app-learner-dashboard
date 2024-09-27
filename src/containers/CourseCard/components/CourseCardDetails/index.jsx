f4f6bca2e6c349f54f542a7e2b1d39bcdd30fd74
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-09-27 12:53:37 +0530

    removed short name for certificate program and staging js and css

--- src/containers/CourseCard/components/CourseCardDetails/index.jsx
+++ src/containers/CourseCard/components/CourseCardDetails/index.jsx
@@ -6,9 +6,14 @@ import { Button } from '@edx/paragon';
 import useCardDetailsData from './hooks';
 import './index.scss';
 import { reduxHooks } from 'hooks';
+import { getConfig } from '@edx/frontend-platform';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import messages from '../../../LearnerDashboardHeader/messages';
 
 const CourseCardDetails = ({ cardId }) => {
   const { homeUrl } = reduxHooks.useCardCourseRunData(cardId);
+  const { formatMessage } = useIntl();
+  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
 
   const extractCourseDetails = () => {
     if (!homeUrl) return { org: '', courseCode: '' };
@@ -37,9 +42,7 @@ const CourseCardDetails = ({ cardId }) => {
     <span className="small" data-testid="CourseCardDetails">
       <div className='quick-link-tag' style={{display:"none"}}> {providerName}</div>
       <div className='course-number-display-string' style={{display:"none"}}>{courseNumber}</div>
-      {
-       courseCode 
-      }
+      {siteNameMessage === "IIT Kanpur eMasters Degree" ? courseCode : '' }
     </span>
   );
 };
