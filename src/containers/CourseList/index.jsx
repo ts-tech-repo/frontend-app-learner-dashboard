7c3e9df318f18983ab78eb412dc733b38070360e
sohail.m <sohail.m@talentsprint.com>
2024-10-25 16:57:38 +0530

    print logs card id

--- src/containers/CourseList/index.jsx
+++ src/containers/CourseList/index.jsx
@@ -45,6 +45,7 @@ export const CourseList = () => {
               </div>
             )}
             <div className="d-flex flex-row flex-grow-1 card_div">
+              {console.log(cardId, visibleList )}
               {visibleList.map(({ cardId }) => (
                 <CourseCard key={cardId} cardId={cardId} />
               ))}
