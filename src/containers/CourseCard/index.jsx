7c8031b67756d1282175036216c73ef164f69f72
sohail.m <sohail.m@talentsprint.com>
2024-10-30 15:00:16 +0530

    rearranged cards

--- src/containers/CourseCard/index.jsx
+++ src/containers/CourseCard/index.jsx
@@ -19,32 +19,28 @@ export const CourseCard = ({
   const isCollapsed = useIsCollapsed();
   const orientation = isCollapsed ? 'vertical' : 'horizontal';
   return (
-    <>
-      {Array.from(Array(cardId.split('-')[1]).keys()).map((index) => (
-        <div key={`card-${index}`} className="mb-4.5 course-card" id={`card-${index}`} data-testid="CourseCard">
-          <Card orientation={orientation}>
-            <div className="d-flex flex-column w-100">
-              <div {...(!isCollapsed && { className: 'd-flex flex-column' })}>
-                <CourseCardImage cardId={`card-${index}`} orientation="horizontal" />
-                <Card.Body>
-                  <Card.Section className="pt-2 pb-0">
-                    <CourseCardDetails cardId={`card-${index}`} />
-                  </Card.Section>
-                  <Card.Header
-                    title={<CourseCardTitle cardId={`card-${index}`} />}
-                    actions={<CourseCardMenu cardId={`card-${index}`} />}
-                  />
-                  <Card.Footer orientation={orientation}>
-                    <CourseCardActions cardId={`card-${index}`} />
-                  </Card.Footer>
-                </Card.Body>
-              </div>
-              {/* <CourseCardBanners cardId={`card-${index}`} /> */}
-            </div>
-          </Card>
+    <div className="mb-4.5 course-card" id={cardId} data-testid="CourseCard" style={{ order: parseInt(cardId.replace('card-', '')) }}>
+      <Card orientation={orientation}>
+        <div className="d-flex flex-column w-100">
+          <div {...(!isCollapsed && { className: 'd-flex flex-column' })}>
+            <CourseCardImage cardId={cardId} orientation="horizontal" />
+            <Card.Body>
+              <Card.Section className="pt-2 pb-0">
+                <CourseCardDetails cardId={cardId} />
+              </Card.Section>
+              <Card.Header
+                title={<CourseCardTitle cardId={cardId} />}
+                actions={<CourseCardMenu cardId={cardId} />}
+              />
+              <Card.Footer orientation={orientation}>
+                <CourseCardActions cardId={cardId} />
+              </Card.Footer>
+            </Card.Body>
+          </div>
+{/*           <CourseCardBanners cardId={cardId} /> */}
         </div>
-      ))}
-    </>
+      </Card>
+    </div>
   );
 };
 CourseCard.propTypes = {
