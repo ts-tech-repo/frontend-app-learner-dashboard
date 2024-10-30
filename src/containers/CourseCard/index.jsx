b3f6969376f9d2954b3247a61f0cf106a3dbb14e
sohail.m <sohail.m@talentsprint.com>
2024-10-30 17:27:54 +0530

    debugging

--- src/containers/CourseCard/index.jsx
+++ src/containers/CourseCard/index.jsx
@@ -13,42 +13,59 @@ import CourseCardTitle from './components/CourseCardTitle';
 
 import './CourseCard.scss';
 
-export const CourseCard = ({
-  cardId,
-}) => {
+export const CourseCard = ({ cardId }) => {
   const isCollapsed = useIsCollapsed();
   const orientation = isCollapsed ? 'vertical' : 'horizontal';
+
   return (
     <div className="mb-4.5 course-card" id={cardId} data-testid="CourseCard">
-      {cardId.split(',').sort().map((id, index) => (
-        <React.Fragment key={index}>
-          <Card orientation={orientation}>
-            <div className="d-flex flex-column w-100">
-              <div {...(!isCollapsed && { className: 'd-flex flex-column' })}>
-                <CourseCardImage cardId={id} orientation="horizontal" />
-                <Card.Body>
-                  <Card.Section className="pt-2 pb-0">
-                    <CourseCardDetails cardId={id} />
-                  </Card.Section>
-                  <Card.Header
-                    title={<CourseCardTitle cardId={id} />}
-                    actions={<CourseCardMenu cardId={id} />}
-                  />
-                  <Card.Footer orientation={orientation}>
-                    <CourseCardActions cardId={id} />
-                  </Card.Footer>
-                </Card.Body>
-              </div>
-{/*               <CourseCardBanners cardId={id} /> */}
-            </div>
-          </Card>
-        </React.Fragment>
-      ))}
+      <Card orientation={orientation}>
+        <div className="d-flex flex-column w-100">
+          <div {...(!isCollapsed && { className: 'd-flex flex-column' })}>
+            <CourseCardImage cardId={cardId} orientation="horizontal" />
+            <Card.Body>
+              <Card.Section className="pt-2 pb-0">
+                <CourseCardDetails cardId={cardId} />
+              </Card.Section>
+              <Card.Header
+                title={<CourseCardTitle cardId={cardId} />}
+                actions={<CourseCardMenu cardId={cardId} />}
+              />
+              <Card.Footer orientation={orientation}>
+                <CourseCardActions cardId={cardId} />
+              </Card.Footer>
+            </Card.Body>
+          </div>
+          {/* <CourseCardBanners cardId={cardId} /> */}
+        </div>
+      </Card>
     </div>
   );
 };
+
 CourseCard.propTypes = {
   cardId: PropTypes.string.isRequired,
 };
 
-export default CourseCard;
+// CourseCardList component that sorts and renders CourseCards in ascending order by cardId
+const CourseCardList = ({ courses }) => {
+  const sortedCourses = [...courses].sort((a, b) => a.cardId.localeCompare(b.cardId));
+
+  return (
+    <div className="course-card-list">
+      {sortedCourses.map(course => (
+        <CourseCard key={course.cardId} cardId={course.cardId} />
+      ))}
+    </div>
+  );
+};
+
+CourseCardList.propTypes = {
+  courses: PropTypes.arrayOf(PropTypes.shape({
+    cardId: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+// Export CourseCard as a named export and CourseCardList as the default export
+export { CourseCard };
+export default CourseCardList;
