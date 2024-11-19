94d95865e62301acac0cd81384fd3a10378d9a44
sohail.m <sohail.m@talentsprint.com>
2024-11-19 14:39:35 +0530

    removed class

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ export const App = () => {
   return (
     <>
     <div style = {{display:"none"}} className="emailAddress">{authenticatedUser.email}</div>
-    <div style = {{display:"none"}} className="d-md-inline userName">{authenticatedUser.username}</div>
+    <div style = {{display:"none"}} className="userName">{authenticatedUser.username}</div>
       <Helmet>
         <title>{formatMessage(messages.pageTitle)}</title>
         <link
