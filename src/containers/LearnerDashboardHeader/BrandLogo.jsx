1dafbcb4de7f75b18b4e517198266e04010bd992
dattatadepalli <datta.t@talentsprint.com>
2024-12-30 17:47:36 +0530

    Update BrandLogo.jsx

--- src/containers/LearnerDashboardHeader/BrandLogo.jsx
+++ src/containers/LearnerDashboardHeader/BrandLogo.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { useIntl } from "@edx/frontend-platform/i18n";
+import { Link } from "react-router-dom";
 
-import { reduxHooks } from 'hooks';
 
-import { getConfig } from '@edx/frontend-platform';
-import messages from './messages';
+import { reduxHooks } from "hooks";
+
+import { getConfig } from "@edx/frontend-platform";
+import messages from "./messages";
 
 export const BrandLogo = () => {
   const { formatMessage } = useIntl();
   const dashboard = reduxHooks.useEnterpriseDashboardData();
 
   return (
-    <a href={dashboard?.url || '/'} className="mx-auto">
+    <Link to={dashboard?.url || "/"} className="mx-auto">
       <img
         className="logo py-3"
         src={getConfig().LOGO_URL}
         alt={formatMessage(messages.logoAltText)}
       />
-    </a>
+    </Link>
   );
 };
 
