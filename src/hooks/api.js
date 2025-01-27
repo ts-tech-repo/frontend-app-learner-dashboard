ade0823529d62fa835b5c23150ad4d626b0e1ca4
sohail.m <sohail.m@talentsprint.com>
2025-01-27 17:51:07 +0530

    updated key

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -29,7 +29,7 @@ export const useInitializeApp = () => {
     requestKey: RequestKeys.initialize,
     onSuccess: ({ data }) => {
       window.ptcSubmitted = data.ptcSubmitted; 
-      window.ptcURL = data.ptcURl; 
+      window.ptcURL = data.ptcURL; 
       loadData(data);
     },
   });
