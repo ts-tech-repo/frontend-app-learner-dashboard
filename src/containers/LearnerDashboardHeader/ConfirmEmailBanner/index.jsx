dc58b9f33e0d01b41f82e1eb769504bdb9b2c4be
sohail.m <sohail.m@talentsprint.com>
2025-01-29 15:10:26 +0530

    removed Remember to confirm your email from top nav bar

--- src/containers/LearnerDashboardHeader/ConfirmEmailBanner/index.jsx
+++ src/containers/LearnerDashboardHeader/ConfirmEmailBanner/index.jsx
@@ -30,20 +30,7 @@ export const ConfirmEmailBanner = () => {
 
   return (
     <>
-      <PageBanner show={showPageBanner} dismissible onDismiss={closePageBanner}>
-        {formatMessage(messages.confirmEmailTextReminderBanner, {
-          confirmNowButton: (
-            <Button
-              className="confirm-email-now-button"
-              variant="link"
-              size="inline"
-              onClick={openConfirmModalButtonClick}
-            >
-              {formatMessage(messages.confirmNowButton)}
-            </Button>
-          ),
-        })}
-      </PageBanner>
+      
       <MarketingModal
         title=""
         isOpen={showConfirmModal}
