e35f1156973d549a5827f66e428a66c11ec88459
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2025-01-30 15:47:53 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState  } from "react";
 import { Helmet } from "react-helmet";
 
 import { useIntl } from "@edx/frontend-platform/i18n";
