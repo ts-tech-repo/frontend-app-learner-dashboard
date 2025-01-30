743d7d35efc5c8ef91c4d67bd7dc5d85e9914cee
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2025-01-30 12:19:32 +0530

    reverted all disable changes

--- src/containers/CourseCard/components/CourseCardTitle.jsx
+++ src/containers/CourseCard/components/CourseCardTitle.jsx
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 import track from 'tracking';
 import { reduxHooks } from 'hooks';
 import useActionDisabledState from './hooks';
+import { getConfig } from '@edx/frontend-platform';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import messages from '../../LearnerDashboardHeader/messages';
 
 const { courseTitleClicked } = track.course;
 
 export const CourseCardTitle = ({ cardId }) => {
+  const { formatMessage } = useIntl();
+  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
+  const { bannerImgSrc } = reduxHooks.useCardCourseData(cardId);
   const { courseName } = reduxHooks.useCardCourseData(cardId);
   const { homeUrl } = reduxHooks.useCardCourseRunData(cardId);
   const extractedCourse = courseName.includes("-") ? courseName.split(/-(.+)/)[1].trim() : courseName;
@@ -22,24 +28,27 @@ export const CourseCardTitle = ({ cardId }) => {
     <h3>
       {disableCourseTitle ? (
         <span className="course-card-title" data-testid="CourseCardTitle">
-          {extractedCourse}
+          {siteNameMessage === "IIT Kanpur eMasters Degree" && bannerImgSrc.includes("marker") 
+            ? courseName 
+            : (siteNameMessage === "IIT Kanpur eMasters Degree" 
+              ? extractedCourse 
+              : courseName)}
         </span>
-      ) : window.ptcSubmitted ? (
+      ) : (
         <a
           href={homeUrl}
           className="course-card-title"
           data-testid="CourseCardTitle"
           onClick={handleTitleClicked}
         >
-          {extractedCourse}
-        </a>
-      ) : (
-        <a className="course-card-title" data-testid="CourseCardTitle">
-          {extractedCourse}
+         {siteNameMessage === "IIT Kanpur eMasters Degree" && bannerImgSrc.includes("marker") 
+            ? courseName 
+            : (siteNameMessage === "IIT Kanpur eMasters Degree" 
+              ? extractedCourse 
+              : courseName)}
         </a>
       )}
     </h3>
-
   );
 };
 
