000018e01e44f24d4735be545f6804929a121398
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2025-01-30 12:33:39 +0530

    disable

--- src/containers/CourseCard/components/CourseCardTitle.jsx
+++ src/containers/CourseCard/components/CourseCardTitle.jsx
@@ -4,16 +4,10 @@ import PropTypes from 'prop-types';
 import track from 'tracking';
 import { reduxHooks } from 'hooks';
 import useActionDisabledState from './hooks';
-import { getConfig } from '@edx/frontend-platform';
-import { useIntl } from '@edx/frontend-platform/i18n';
-import messages from '../../LearnerDashboardHeader/messages';
 
 const { courseTitleClicked } = track.course;
 
 export const CourseCardTitle = ({ cardId }) => {
-  const { formatMessage } = useIntl();
-  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
-  const { bannerImgSrc } = reduxHooks.useCardCourseData(cardId);
   const { courseName } = reduxHooks.useCardCourseData(cardId);
   const { homeUrl } = reduxHooks.useCardCourseRunData(cardId);
   const extractedCourse = courseName.includes("-") ? courseName.split(/-(.+)/)[1].trim() : courseName;
@@ -28,27 +22,24 @@ export const CourseCardTitle = ({ cardId }) => {
     <h3>
       {disableCourseTitle ? (
         <span className="course-card-title" data-testid="CourseCardTitle">
-          {siteNameMessage === "IIT Kanpur eMasters Degree" && bannerImgSrc.includes("marker") 
-            ? courseName 
-            : (siteNameMessage === "IIT Kanpur eMasters Degree" 
-              ? extractedCourse 
-              : courseName)}
+          {extractedCourse}
         </span>
-      ) : (
+      ) : window.ptcSubmitted ? (
         <a
           href={homeUrl}
           className="course-card-title"
           data-testid="CourseCardTitle"
           onClick={handleTitleClicked}
         >
-         {siteNameMessage === "IIT Kanpur eMasters Degree" && bannerImgSrc.includes("marker") 
-            ? courseName 
-            : (siteNameMessage === "IIT Kanpur eMasters Degree" 
-              ? extractedCourse 
-              : courseName)}
+          {extractedCourse}
+        </a>
+      ) : (
+        <a className="course-card-title" data-testid="CourseCardTitle">
+          {extractedCourse}
         </a>
       )}
     </h3>
+
   );
 };
 
